1479c7746fb87e892061776b4586d415cdcc1a35
NerilTm <149512858+NerilTm@users.noreply.github.com>
2024-05-24 19:24:46 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'neril-back-adminka.nomoredomainswork.ru/api';
+export const BASE_URL = 'https://neril-back-adminka.nomoredomainswork.ru/api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
